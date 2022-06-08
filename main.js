797c83020a49c904c796de14a2591f7a2469cfe9
EmmyCodes <emingram49@gmail.com>
2022-06-07 19:12:00 -0600

    added new functionality

--- main.js
+++ main.js
@@ -1,17 +1,57 @@
 // $ means this function is not a pure function, use with caution
 
-function createReminderHtml$({text, complete}) {
+const remCompleteEvent = new Event("remcomplete", {bubbles: true});
+const remDeleteEvent = new Event("remdelete", {bubbles: true});
+
+function createCompleteButton(id, complete) {
+    const completeBtn = document.createElement("button");
+    completeBtn.innerHTML = complete ? "check" : "uncheck";
+    completeBtn.remData = {id: id, complete: complete};
+    completeBtn.addEventListener("click", function(e) {
+        e.target.dispatchEvent(remCompleteEvent);
+    });
+    return completeBtn;
+}
+
+function createDeleteButton(id) {
+    const deleteBtn = document.createElement("button");
+    deleteBtn.innerHTML = "delete";
+    deleteBtn.remData = {id: id};
+    deleteBtn.addEventListener("click", function(e) {
+        e.target.dispatchEvent(remDeleteEvent);
+    });
+    return deleteBtn;
+}
+
+function createReminderHtml({id, text, complete}) {
     const remContainer = document.createElement("li");
     const textContainer = document.createElement("p");
-    const completeBtn = document.createElement("button");
+    const completeBtn = createCompleteButton(id, complete);
+    const deleteBtn = document.createElement("button");
 
     textContainer.innerHTML = text;
-    completeBtn.innerHTML = complete ? "check" : "uncheck";
-
+    deleteBtn.innerHTML = "delete";
+    
     remContainer.appendChild(textContainer);
     remContainer.appendChild(completeBtn);
+    remContainer.appendChild(deleteBtn);
 
     return remContainer;
 }
 
-export {createReminderHtml$};
+function createReminderListHtml(remHtmlFunct, reminderData) {
+    const remList = document.createElement("ul");
+    reminderData.forEach(remObj => {
+        const remHtml = remHtmlFunct(remObj);
+        remList.appendChild(remHtml);
+    });
+    return remList;
+}
+
+function updateComplete(reminders, {id, complete}) {
+    const targetIndex = reminders.findIndex((rem) => rem.id === id);
+    reminders[targetIndex]["complete"] = !complete;
+    return reminders;
+}
+
+export {createReminderHtml, createReminderListHtml, updateComplete};
