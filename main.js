15002854f8e428938c21de0ab31b8ddf44497323
EmmyCodes <emingram49@gmail.com>
2022-07-07 10:48:22 -0600

    removed optional side effects

--- main.js
+++ main.js
@@ -2,48 +2,64 @@ const remCompleteEvent = new Event("remcomplete", {bubbles: true});
 const remDeleteEvent = new Event("remdelete", {bubbles: true});
 
 class RemViewBuilder {
-    constructor() {
-        this.buildList = [];
+    constructor(buildList=[]) {
+        this.buildList = buildList;
+    }
+
+    getBuildList() {
+        return this.buildList.length > 0 ? this.buildList.map((x) => x) : [];
     }
 
     createNode(tag) {
-        this.pushNode(document.createElement(tag));
-        return this;
+        return this.pushNode(document.createElement(tag));
     }
 
     setNodeText(text) {
-        this.getCurrentNode().innerHTML = text;
-        return this;
+        const currentIndex = this.getCurrentNodeIndex();
+        const newBuildList = this.getBuildList();
+        newBuildList[currentIndex].innerHTML = text;
+        return new RemViewBuilder(newBuildList);
     }
 
     pushNode(node) {
-        this.buildList.push(node);
-        return this;
+        const newBuildList = this.getBuildList();
+        newBuildList.push(node);
+        return new RemViewBuilder(newBuildList);
+    }
+
+    pushNodes(nodes) {
+        const newBuildList = this.getBuildList();
+        nodes.forEach((n) => newBuildList.push(n));
+        return new RemViewBuilder(newBuildList);
     }
 
     setNodeRemData(data) {
-        this.getCurrentNode().remData = data;
-        return this;
+        const currentIndex = this.getCurrentNodeIndex();
+        const newBuildList = this.getBuildList();
+        newBuildList[currentIndex].remData = data;
+        return new RemViewBuilder(newBuildList);
     }
 
     setDispatchOnClick(event) {
-        this.getCurrentNode().addEventListener("click", (e) => e.target.dispatchEvent(event));
-        return this;
+        const currentIndex = this.getCurrentNodeIndex();
+        const newBuildList = this.getBuildList();
+        newBuildList[currentIndex].addEventListener("click", (e) => e.target.dispatchEvent(event));
+        return new RemViewBuilder(newBuildList);
     }
 
-    getCurrentNode() {
-        return this.buildList[this.buildList.length - 1];
+    getCurrentNodeIndex() {
+        return this.buildList.length - 1;
     }
 
     assemble() {
-        const root = this.buildList.shift();
-        this.buildList.forEach((node) => root.appendChild(node));
-        this.buildList = [root];
-        return this;
+        const newBuildList = this.getBuildList();
+        const root = newBuildList.shift();
+        newBuildList.forEach((node) => root.appendChild(node));
+        return new RemViewBuilder([root]);
     }
 
     popNode() {
-        return this.buildList.pop();
+        return this.getBuildList().pop();
     }
 }
 
@@ -78,12 +94,8 @@ function createReminderNode({id, text, complete}) {
 
 function createReminderListNode(reminderData) {
     const viewBuilder = new RemViewBuilder();
-    viewBuilder.createNode("ul");
-    reminderData.forEach(remObj => {
-        viewBuilder.pushNode(createReminderNode(remObj));
-    });
-    
-    return viewBuilder.assemble().popNode();
+    const remNodes = reminderData.map(createReminderNode);
+    return viewBuilder.createNode("ul").pushNodes(remNodes).assemble().popNode();
 }
 
 class ReminderList {
